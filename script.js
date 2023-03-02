6ba88d03c06fdc06081cbeec18f3a57d2b70c2b7
daudi-dev <waumini@gmail.com>
2023-03-02 23:11:56 +0300

    Created divs inside the DOM, updated stylesheet

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const div = document.createElement("div");
-body.appendChild(div);
-body.appendChild(div);
-body.appendChild(div);
+
+    let newDiv = document.createElement('div');
+
+    console.log(newDiv);
+
+    let text = document.createTextNode('this is new div');
+
+    console.log(text);
+
+    newDiv.appendChild(text);
+
+    console.log(newDiv);
+
+    let container = document.querySelector('#container');
+      for (let i = 1; i < 16; i++) {
+            let d = document.createElement('div');
+            
+            container.appendChild(d);
+            
+        
+        }
+        console.log(container);
+       
+
+    document.getElementById('container').appendChild(divs);
+
