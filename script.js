cf0b2d9e66da875c721403b1c851752ed192989b
daudi-dev <waumini@gmail.com>
2023-03-05 23:25:29 +0300

    reworked for loop to create 16divs && container height and width/ justify content spaced around

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
     console.log(newDiv);
 
     let container = document.querySelector('#container');
-      for (let i = 1; i < 16; i++) {
+      for (let i = 0; i < 16; i++) {
             let d = document.createElement('div');
             
             container.appendChild(d);
